f1c14f7495af73dd1af7f7c84e6efae5607ffbe7
Devon Troedel <devon.troedel@gmail.com>
2019-04-18 16:12:34 -0500

    fixing error page location

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<v-container>
-			<v-layout fill-height justify-center>
+			<v-layout class='mt-5' fill-height justify-center>
 				<h1 v-if='error.statusCode === 404'>Page not found</h1>
 				<h1 v-else>An error occurred</h1>
 			</v-layout>
