8b7ed1ea9e5139ca2f691de2d43a576a5d48bc25
Devon Troedel <devon.troedel@gmail.com>
2019-04-18 16:51:56 -0500

    editing colors a bit

--- components/nav/TheNavDrawer.vue
+++ components/nav/TheNavDrawer.vue
@@ -1,23 +1,23 @@
 <template>
 	<v-navigation-drawer
-		:mini-variant='miniDrawer'
+		:mini-variant="miniDrawer"
 		app
-		class='secondary'
+		class="secondary"
 		dark
-		v-model='drawer'
-		width='200'
+		v-model="drawer"
+		width="200"
 	>
 		<v-layout column fill-height>
-			<v-list class='pt-0'>
-				<v-list-group v-model='navList'>
+			<v-list class="pt-0">
+				<v-list-group v-model="navList">
 					<template v-slot:activator>
 						<v-list-tile>
 							<v-list-tile-content>
-								<v-list-tile-title :class='{"tertiary--text": navList}'>Navigation</v-list-tile-title>
+								<v-list-tile-title :class="{'primary--text': navList}">Navigation</v-list-tile-title>
 							</v-list-tile-content>
 						</v-list-tile>
 					</template>
-					<v-list-tile :key='index' :to='link.route' nuxt v-for='(link, index) in links'>
+					<v-list-tile :key="index" :to="link.route" nuxt v-for="(link, index) in links">
 						<v-list-tile-action>
 							<v-icon>{{ link.icon }}</v-icon>
 						</v-list-tile-action>
@@ -30,15 +30,15 @@
 			</v-list>
 
 			<v-container>
-				<v-layout align-content-end class='text-xs-center' fill-height justify-space-around wrap>
-					<v-flex :key='index' v-for='(contact, index) in contacts'>
-						<v-tooltip color='primary grey--text text--darken-4' top>
-							<template v-slot:activator='{ on }'>
-								<v-btn class='px-0 mx-0' icon v-on='on'>
+				<v-layout align-content-end class="text-xs-center" fill-height justify-space-around wrap>
+					<v-flex :key="index" v-for="(contact, index) in contacts">
+						<v-tooltip color="primary tertiary--text" top>
+							<template v-slot:activator="{ on }">
+								<v-btn class="px-0 mx-0" icon v-on="on">
 									<v-icon>{{ contact.icon }}</v-icon>
 								</v-btn>
 							</template>
-							<span class='text-capitalize'>{{ contact.tip }}</span>
+							<span class="text-capitalize">{{ contact.tip }}</span>
 						</v-tooltip>
 					</v-flex>
 				</v-layout>
